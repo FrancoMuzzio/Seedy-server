a201cd91771aa933b374ebfd6929dccd9f33783d
Franco Muzzio <48494284+FrancoMuzzio@users.noreply.github.com>
2023-08-31 13:55:40 -0300

    Instalacion de jwt crypto

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     port: 587,
     auth: {
-        user: 'aileen.bode@ethereal.email',
-        pass: 'sgHhseucU4m59WpdVY'
+        user: 'isidro.erdman@ethereal.email',
+        pass: 'fTYa3GhqnRM2T4Q8Gy'
     }
 });
 
