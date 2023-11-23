8c2ff894604515ccd1c05dba9d46782b57126b54
Franco Muzzio <48494284+FrancoMuzzio@users.noreply.github.com>
2023-11-22 22:01:36 -0300

    log

--- src/controllers/plantController.js
+++ src/controllers/plantController.js
@@ -157,7 +157,9 @@ exports.getUserPlants = async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10; // Establecer un límite predeterminado
     const offset = (page - 1) * limit;
-
+    console.log(page);
+    console.log(limit);
+    console.log(offset);
     const userWithPlants = await User.findByPk(userId, {
       include: [
         {
