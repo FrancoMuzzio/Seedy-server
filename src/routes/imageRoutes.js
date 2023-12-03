f3da5d95c383fda598a6bc239af91792ed88ab1a
Franco Muzzio <48494284+FrancoMuzzio@users.noreply.github.com>
2023-12-03 14:29:43 -0300

    log

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -75,6 +75,7 @@ router.post(
     if (!req.file) {
       return res.status(400).send("No image uploaded");
     }
+    console.log(req.file.originalname);
     res.status(200).send({
       imageUrl: `/uploads/${decodeURI(req.params.folderName)}/${
         req.file.originalname
